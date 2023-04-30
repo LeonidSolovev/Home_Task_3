874258a646efc712e7b0531ac89638083d49454e
Leonid Solovev <ls3112@yandex.ru>
2023-04-30 14:26:58 +0300

    Task 19 done

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -2,3 +2,47 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+int num;
+
+num = GetUserInput("Введите пятизначное число: ");
+num = GetCorrectNumber(num);
+
+int num1 = num/10000;
+int num2 = (num%10000)/1000;
+int num4 = (num%100)/10;
+int num5 = num%10;
+
+if (num1 == num5 && num2 == num4)
+    Console.WriteLine($"{num} -> да");
+else
+    Console.WriteLine($"{num} -> нет");
+
+
+
+static int GetUserInput(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+    
+}
+
+static int GetCorrectNumber(int number)
+{
+    
+while (number <= 9999) 
+{     
+    Console.WriteLine("Слишком маленькое число!"); 
+    number = GetUserInput("Введите пятизначное число: ");
+    return number;
+}
+
+while (number > 99999)
+{     
+    Console.WriteLine("Слишком большое число!"); 
+    number = GetUserInput("Введите пятизначное число: ");
+    return number;
+}
+    return number;    
+}
